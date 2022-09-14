328d2a374f064d69669fdd913d6ffa41d4d5a6c2
sose1102 <sose1102@gmail.com>
2022-09-14 16:33:16 +0200

    lee fin

--- lectura/lee.py
+++ lectura/lee.py
@@ -3,12 +3,12 @@ from typing import TextIO
 
 def read_data(f: TextIO) -> list[int]:
     lines = f.readlines()
-    return [int(lines) for line in lines]
+    return [int(line) for line in lines]
 
 def show_results(nums: list[int]):
     for num in nums:
         print(num)
 
-if __name__ == "__main":
+if __name__ == "__main__":
     nums = read_data(sys.stdin)
     show_results(nums)
